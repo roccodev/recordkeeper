b4843b622b0431dde63750d377e27c1cc6f1112f
RoccoDev <hey@rocco.dev>
2023-09-03 03:47:49 +0200

    meta: add build script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Syntax:
+# ./build.sh <bdat dir> <webapp output dir>
+
+cwd=$(pwd)
+commit=$(git describe --always --dirty)
+commitmsg=$(git log -1 --pretty=%B)
+
+# Run app builder and pass arguments
+cargo run --release -p app-builder -- $1 webapp/res
+
+# Build webapp after reading game data
+cd webapp
+trunk build --release --public-url recordkeeper
+
+cd dist
+
+# GH pages: catch 404s for links (technically not necessary since
+# Recordkeeper uses an in-memory router
+cp index.html 404.html
+
+# Prepare for deploy (this script doesn't actually deploy, though)
+git init
+git checkout --force -b gh-pages
+git add --all
+git commit -m "[$commit] $commitmsg"
+
+cd $cwd
+
+
